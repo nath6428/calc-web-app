f9dbbabf8f27c8c929ad908e6147e3e62ed5d5f7
Nathan Crasto <nathancrasto6@gmail.com>
2023-04-30 20:10:54 +0400

    Completed

--- calc.js
+++ calc.js
@@ -1,29 +1,87 @@
-function add(data, input){
-    
-    data += input;
-    return data;
+var operationMode = false;
+var data = document.querySelector('.data');
+var input = document.querySelector('.input');
+var numberInputs = Array.from(document.querySelectorAll('.number-button'));
+var deleteInput = document.querySelector('.delete-button')
+var clearInput = document.querySelector('.clear-button')
+var equalInput = document.querySelector('.equal-button')
+var operationInputs = Array.from(document.querySelectorAll('.operation-button'));
 
+/* Delete button functionality */
+deleteInput.addEventListener('click', () => {
+
+    input.innerText = input.innerText.slice(0,input.innerText.length-1)
+
+})
+
+/* Clear button functionality */
+clearInput.addEventListener('click', () => {
+
+    input.innerText = ''
+    data.innerText = ''
+
+})
+
+
+/* Equal button functionality */
+equalInput.addEventListener('click', () => {
+    
+    if (operationMode == true){
+        data.innerText += input.innerText
+        data.innerText = eval(data.innerText)
+        input.innerText = ''
+        operationMode = false
+
+    }
+    else {
+        data.innerText = input.innerText
+        input.innerText = ''
+    }
+})
+
+/* adds numbers to display (used upon number click)*/
+function addToInput(){
+    input.innerText += this.textContent
 }
 
-function subtract(data, input){
-    
-    data -= input;
-    return data;
-
+/* All numbers button functionality */
+for (i in numberInputs){
+    numberInputs[i].addEventListener('click', addToInput)
 }
 
+function operationEvent(op){
 
-function multiply(data, input){
-    
-    data *= input;
-    return data;
-
+    data.innerText = input.innerText
+    input.innerText = `${op}` + ''
 }
 
+for (i in operationInputs){
 
-function divide(data, input){
+    var operation = operationInputs[i]
+    operation.addEventListener('click', () => {operationMode = true})
+
+    switch (operation.textContent) {
+        
+        case '+':
+            operation.addEventListener('click', () => {operationEvent('+')})
+            break;
+        
+        case '-':
+            operation.addEventListener('click', () => {operationEvent('-')})
+            break;
+
+        case '×':
+            operation.addEventListener('click', () => {operationEvent('*')})
+            break;
+
+        case '÷':
+            operation.addEventListener('click', () => {operationEvent('/')})
+            break;
     
-    input == 0 ? data = "Cannot divide by zero." : data /= input;
-    return data;
+        default:
+            break;
+    }
+    
 }
+
 
